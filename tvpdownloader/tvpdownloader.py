bb1cb77fcf06a77ab801a731536ffb9f1fa5fff2
Patrick Kubiak <patrick@antuple.net>
2018-06-05 15:05:03 -0400

    option to set quality

--- tvpdownloader/tvpdownloader.py
+++ tvpdownloader/tvpdownloader.py
@@ -8,9 +8,12 @@ import filetype
 from mimetypes import guess_extension
 
 class TVPDownloader:
-  def __init__(self, url, dest="downloads/"):
+  def __init__(self, url, dest="downloads/", quality=5):
     self.url = url
     self.dest = dest
+
+    # quality can be 2 to 7
+    self.quality = str(quality)
 
     # check destination
     if not os.path.exists(self.dest):
@@ -38,15 +41,17 @@ class TVPDownloader:
     episode_data = requests.get(self.episode_url).text
     episode_soup = BeautifulSoup(episode_data, 'html.parser')
     episode_script = episode_soup.find_all("script")[9].text
-    self.episode_links = re.findall("src:\'(.*?)\'", episode_script)
-    #for link in self.episode_links:
-    #  print("Episode link = " + link)
-    return self.episode_links
+    self.episode_link = re.findall("src:\'(.*?)\', type: \'video/mp4\'", episode_script)[0]
+
+    # replace quality
+    self.episode_link = self.episode_link.replace("video-5", "video-" + self.quality)
+    #print("Episode link = " + self.episode_link)
+    return self.episode_link
 
   def parse_output_name(self):
     series = re.search("\/([^/,]+)\,", self.url).group(1)
     episode = re.search("\,(.*?)\,", self.url).group(1)
-    self.out_name = series + " - " + episode
+    self.out_name = series + " - " + episode + " - " + self.quality
     #print("Output name = " + self.out_name)
     return self.out_name
 
@@ -54,37 +59,36 @@ class TVPDownloader:
     # download
     self.find_player()
     self.find_episode()
-    episode_links = self.find_episode_link()
+    episode_link = self.find_episode_link()
     output_name = self.parse_output_name()
 
-    for link in episode_links:
-      # determine episode file type
-      mimetype = urllib.request.urlopen(link).info().get_content_type()
-      #print("Content-Type = " + mimetype)
+    # determine episode file type
+    mimetype = urllib.request.urlopen(episode_link).info().get_content_type()
+    #print("Content-Type = " + mimetype)
 
-      # guess episode extension
-      extension = guess_extension(mimetype)
-      #print("Guessed extension = " + extension)
+    # guess episode extension
+    extension = guess_extension(mimetype)
+    #print("Guessed extension = " + extension)
 
-      # combine output path
-      output_path = os.path.join(self.dest, output_name + extension)
+    # combine output path
+    output_path = os.path.join(self.dest, output_name + extension)
 
-      # write to file
-      print("Writing " + output_path)
-      # https://stackoverflow.com/questions/7243750/download-file-from-web-in-python-3
-      with urllib.request.urlopen(link) as response, open(output_path, 'wb') as out_file:
-        shutil.copyfileobj(response, out_file)
+    # write to file
+    print("Writing " + output_path)
+    # https://stackoverflow.com/questions/7243750/download-file-from-web-in-python-3
+    with urllib.request.urlopen(episode_link) as response, open(output_path, 'wb') as out_file:
+      shutil.copyfileobj(response, out_file)
 
 if __name__ == '__main__':
   # list of urls to download
   urls = [
-    "https://vod.tvp.pl/video/rodzinkapl,odc1,3994796",
-    "https://vod.tvp.pl/video/rodzinkapl,odc-221,34842411",
+    {"url": "https://vod.tvp.pl/video/rodzinkapl,odc1,3994796", "quality": 5},
+    {"url": "https://vod.tvp.pl/video/rodzinkapl,odc-221,34842411", "quality": 5},
   ]
 
-  for url in urls:
+  for u in urls:
     # download each url
-    print("Downloading " + url)
-    TVPDownloader(url).get()
+    print("Downloading: " + u['url'] + " Quality: " + str(u['quality']))
+    TVPDownloader(url=u['url'], quality=u['quality']).get()
 
   print("Done!")
